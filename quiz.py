e25f704e9ce08e2d534333364dac008f6f49592b
Alan Campbell <ahcampbellster@gmail.com>
2017-10-19 20:25:03 -0700

    Simplified quiz.py, removed some code duplication and encapsulated key elements of the module in functions.

--- quiz.py
+++ quiz.py
@@ -3,81 +3,67 @@ from display import quiz_object_builder, save_to_html
 from renderers import*
 from pathlib import Path
 from Quiz_Input3 import TAG
+
 parser = argparse.ArgumentParser(description= 'Command Line Quiz Display Utility')
 parser.add_argument('filename', nargs = '*', help='Enter the exact filenames for the quizzes you want to open, be sure they are compatible with the renderer methods..')
 parser.add_argument('--type', help='Enter clean, c, rubric, or r.')
 parser.add_argument('--mode', help='Enter html, h, basic, or b.')
 args = parser.parse_args()
-#True = Display Rubric, False = Hide Rubric
-if args.type in ('rubric', 'r'):
-    is_clean = True
+args.mode = args.mode.lower()
+if args.mode not in ('h', 'html', 'b', 'basic'):
+    print(f'{args.mode.upper()} is not valid, please enter h, html, basic, or b.')
 else:
-    is_clean = False
+    # True = Display Rubric, False = Hide Rubric
+    if args.type in ('rubric', 'r'):
+        is_clean = True
+    else:
+        is_clean = False
 
+    if len(args.filename) == 1:
+        directory = args.filename[0]
+    else:
+        directory = None
+    if directory:
+        b = Path(directory).is_dir()
+    else:
+        b = False
 
+    def hamburgler_helper(args_mode, quiz_object):
+        if args_mode in ('basic', 'b'):
+            instance = TextQuizRenderer()
+            display_method = instance.render(quiz_object, is_clean)
+            print(display_method)
+        elif args_mode in ('html', 'h'):
+            instance = HTMLQuizRender()
+            display_method = instance.render(quiz_object, is_clean)
+            save_to_html(f'{i}.html', display_method)
+            os.startfile(f'{i}.html')
 
-#file_list = []
-#if args.filename == a_directory:
-#   for i in a_directory:
-#       if i.tag == current_tag:
-#           filelist.append(i.name)
-
-mode = None
-
-#Behavior:  if args.filename is a path object:  for each file in the directory, check if the file has a valid tag.
-
-if len(args.filename) == 1:
-    directory = args.filename[0]
-else:
-    directory = None
-
-if directory:
-    b = Path(directory).is_dir()
-else:
-    b = False
-
-if b:
-    just_json = [x for x in Path(directory).iterdir() if x.name.endswith('.json')]
-    for i in just_json:
-        with open(i, 'r') as fp:
+    def tag_check(file_name):
+        with open(file_name, 'r') as fp:
             quiz_json = json.load(fp)
             quiz_object = quiz_object_builder(quiz_json)
-            if quiz_object.tag == TAG:
-                if args.mode.lower() in ('basic', 'b'):
-                    instance = TextQuizRenderer()
-                    display_method = instance.render(quiz_object, is_clean)
-                    print(display_method)
-                elif args.mode.lower() in ('html', 'h'):
-                    instance = HTMLQuizRender()
-                    display_method = instance.render(quiz_object, is_clean)
-                    save_to_html(f'{i}.html', display_method)
-                    os.startfile(f'{i}.html')
-                else:
-                    print(f'{args.mode.upper()} is not valid, please enter h, html, basic, or b.')
-                    break
-else:
-    for i in args.filename:
-        with open(i, 'r') as fp:
-            quiz_json = json.load(fp)
-            quiz_object = quiz_object_builder(quiz_json)
-            if args.mode.lower() in ('basic', 'b'):
-                instance = TextQuizRenderer()
-                display_method = instance.render(quiz_object, is_clean)
-                print(display_method)
-            elif args.mode.lower() in ('html', 'h'):
-                instance = HTMLQuizRender()
-                display_method = instance.render(quiz_object, is_clean)
-                save_to_html(f'{i}.html', display_method)
-                os.startfile(f'{i}.html')
-            else:
-                print(f'{args.mode.upper()} is not valid, please enter h, html, basic, or b.')
-                break
+        if quiz_object.tag == TAG:
+            return quiz_object
+        else:
+            return None
+
+    if b:
+        just_json = [x for x in Path(directory).iterdir() if x.name.endswith('.json')]
+        for i in just_json:
+            if tag_check(i):
+                hamburgler_helper(args.mode, tag_check(i))
+
+    else:
+        for i in args.filename:
+            with open(i, 'r') as fp:
+                quiz_json = json.load(fp)
+                quiz_object = quiz_object_builder(quiz_json)
+                hamburgler_helper(args.mode, quiz_object)
 
 
 
 
 
 
-# args: --type (clean or rubric), --mode (basic, html), filename(s) or quiz files in currnt directory
 
-#TODO:  Add functionality to display all quizzes with the correct Tag.
